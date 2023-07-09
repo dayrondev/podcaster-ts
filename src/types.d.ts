f329172d3e7f8ea36a9fbda30e84bab3b8dfc15c
dayrondev <dayrondev@gmail.com>
2023-07-09 22:17:47 +0200

    Adding types for podcast details response

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,6 +6,14 @@
   description: string
 }
 
+export interface PodcastDetailItem {
+  id: number
+  name: string
+  date: Date
+  time: number
+}
+
+// iTunes response
 export interface ITunesResponse {
   feed: Feed
 }
@@ -143,4 +151,112 @@ export enum Rel {
 
 export enum Type {
   TextHTML = 'text/html',
+}
+
+// Podcast detail response
+export interface PodcastDetailResponseRaw {
+  contents: string
+  status: Status
+}
+
+export interface Status {
+  url: string
+  content_type: string
+  http_code: number
+  response_time: number
+  content_length: number
+}
+
+export interface PodcastResponseResult {
+  resultCount: number
+  results: PodcastDetailResponse[]
+}
+
+export interface PodcastDetailResponse {
+  wrapperType: WrapperType
+  kind: Kind
+  artistId?: number
+  collectionId: number
+  trackId: number
+  artistName?: string
+  collectionName: Name
+  trackName: string
+  collectionCensoredName?: Name
+  trackCensoredName?: Name
+  artistViewUrl: string
+  collectionViewUrl: string
+  feedUrl: string
+  trackViewUrl: string
+  artworkUrl30?: string
+  artworkUrl60: string
+  artworkUrl100?: string
+  collectionPrice?: number
+  trackPrice?: number
+  collectionHdPrice?: number
+  releaseDate: Date
+  collectionExplicitness?: string
+  trackExplicitness?: string
+  trackCount?: number
+  trackTimeMillis: number
+  country: Country
+  currency?: string
+  primaryGenreName?: PrimaryGenreNameEnum
+  contentAdvisoryRating: ContentAdvisoryRating
+  artworkUrl600: string
+  genreIds?: string[]
+  genres: Array<GenreClass | string>
+  closedCaptioning?: ClosedCaptioning
+  description?: string
+  shortDescription?: string
+  artistIds?: number[]
+  episodeContentType?: EpisodeContentType
+  artworkUrl160?: string
+  episodeFileExtension?: EpisodeFileExtension
+  previewUrl?: string
+  episodeUrl?: string
+  episodeGuid?: string
+}
+
+export enum ClosedCaptioning {
+  None = 'none',
+}
+
+export enum Name {
+  TheJoeBuddenPodcast = 'The Joe Budden Podcast',
+}
+
+export enum ContentAdvisoryRating {
+  Clean = 'Clean',
+  Explicit = 'Explicit',
+}
+
+export enum Country {
+  Usa = 'USA',
+}
+
+export enum EpisodeContentType {
+  Audio = 'audio',
+}
+
+export enum EpisodeFileExtension {
+  Mp3 = 'mp3',
+}
+
+export interface GenreClass {
+  name: PrimaryGenreNameEnum
+  id: string
+}
+
+export enum PrimaryGenreNameEnum {
+  Music = 'Music',
+}
+
+export enum Kind {
+  Podcast = 'podcast',
+  PodcastEpisode = 'podcast-episode',
+}
+
+export enum WrapperType {
+  PodcastEpisode = 'podcastEpisode',
+  Track = 'track',
 }
