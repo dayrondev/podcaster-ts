31a9e61aa0d59fffdc413c90d9096799e8e0e422
dayrondev <dayrondev@gmail.com>
2023-07-09 22:21:06 +0200

    Updating hooks

---
+++ src/hooks/usePodcastDetail.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import { getPodcastDetail } from '../services/itunes'
+import { type PodcastDetailItem } from '../types'
+
+interface PodcasterDetailCache {
+  expiration: number
+  details: PodcastDetailItem[]
+}
+
+const PODCASTER_DETAIL_KEY = 'podcaster-datails'
+
+export const usePodcastDetail = (id: string): { details: PodcastDetailItem[] } => {
+  const [details, setDetails] = useState<PodcastDetailItem[]>([])
+
+  useEffect(() => {
+    let data: PodcasterDetailCache | null = null
+    const cacheData = localStorage.getItem(`${PODCASTER_DETAIL_KEY}-${id}`)
+    if (cacheData != null) data = JSON.parse(cacheData)
+
+    if (data != null && new Date().getTime() < data.expiration) {
+      setDetails(data.details)
+    } else {
+      getPodcastDetail(id)
+        .then(details => {
+          // Expires in 1 day
+          const expiration = new Date().getTime() + 1000 * 60 * 60 * 24
+          const data = { expiration, details }
+          localStorage.setItem(`${PODCASTER_DETAIL_KEY}-${id}`, JSON.stringify(data))
+          setDetails(details)
+        })
+        .catch(error => { console.error(error) })
+    }
+  }, [])
+
+  return { details }
+}
--- src/hooks/usePodcasts.tsx
+++ src/hooks/usePodcasts.tsx
@@ -9,7 +9,10 @@ interface PodcasterCache {
 
 const PODCASTER_KEY = 'podcaster'
 
-export const usePodcasts = (search: string): { podcasts: Podcast[] } => {
+export const usePodcasts = (search: string): {
+  podcasts: Podcast[]
+  getPodcastById: (id: string) => Podcast | undefined
+} => {
   const [podcasts, setPodcasts] = useState<Podcast[]>([])
 
   useEffect(() => {
@@ -32,11 +35,17 @@ export const usePodcasts = (search: string): { podcasts: Podcast[] } => {
     }
   }, [])
 
-  return {
-    podcasts: search.length > 0
+  const getFilteredPodcast = (): Podcast[] =>
+    search.length > 0
       ? podcasts.filter(item =>
         item.author.toLowerCase().includes(search.toLowerCase()) ||
         item.title.toLowerCase().includes(search.toLowerCase()))
       : podcasts
+
+  const getPodcastById = (id: string): Podcast | undefined => podcasts.find(item => item.id === id)
+
+  return {
+    podcasts: getFilteredPodcast(),
+    getPodcastById
   }
 }
