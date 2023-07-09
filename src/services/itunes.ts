46db36ab52f094265d2a0fa84ec7e1383c79ee9e
dayrondev <dayrondev@gmail.com>
2023-07-09 22:19:34 +0200

    Update itunes service

--- src/services/itunes.ts
+++ src/services/itunes.ts
@@ -1,6 +1,14 @@
-import { type Podcast, type ITunesResponse } from '../types'
+import {
+  type Podcast,
+  type ITunesResponse,
+  type PodcastDetailResponse,
+  type PodcastDetailResponseRaw,
+  type PodcastResponseResult,
+  type PodcastDetailItem
+} from '../types'
 
-const PODCASTS_API = 'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json'
+const ITUNES_URL = 'https://itunes.apple.com'
+const ALL_ORIGINS_URL = 'https://api.allorigins.win/get?url='
 
 const getMappedPodcasts = (data: ITunesResponse): Podcast[] => {
   return data.feed.entry.map(item => {
@@ -10,15 +18,38 @@ const getMappedPodcasts = (data: ITunesResponse): Podcast[] => {
     const title = item['im:name'].label
     const author = item['im:artist'].label
     const description = item.summary.label
+
     return { id, image, title, author, description }
   })
 }
 
 export const getPopularPodcasts = async (): Promise<Podcast[]> => {
   try {
-    const res = await fetch(PODCASTS_API)
+    const res = await fetch(`${ITUNES_URL}/us/rss/toppodcasts/limit=100/genre=1310/json`)
     const data: ITunesResponse = await res.json()
     return getMappedPodcasts(data)
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
+
+const getMappedPodcastDetail = (results: PodcastDetailResponse[]): PodcastDetailItem[] => {
+  return results.map(item => {
+    const { trackId: id, trackName: name, releaseDate: date, trackTimeMillis: time } = item
+    return { id, name, date, time }
+  })
+}
+
+export const getPodcastDetail = async (id: string): Promise<PodcastDetailItem[]> => {
+  const podcastDetailUrl = `${ITUNES_URL}/lookup?id=${id}&media=podcast&entity=podcastEpisode&limit=20`
+  const url = `${ALL_ORIGINS_URL}${encodeURIComponent(podcastDetailUrl)}`
+
+  try {
+    const res = await fetch(url)
+    const data: PodcastDetailResponseRaw = await res.json()
+    const responseResult: PodcastResponseResult = JSON.parse(data.contents)
+    return getMappedPodcastDetail(responseResult.results)
   } catch (error) {
     console.error(error)
     return []
